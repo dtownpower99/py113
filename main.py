f896de65cb15c689af2146d80f16e00d8ed7c480
Andrew <ajhanus@gmail.com>
2021-12-15 20:34:41 -0500

    added new endpoint and updated readme

--- main.py
+++ main.py
@@ -115,6 +115,10 @@ async def random_joke():
 async def joke_of_the_year():
     return JOKES[0]
 
+@app.get("/number2") 
+async def number2():
+    return JOKES[1]
+
 @app.get("/dumbest") 
 async def dumbest():
     return JOKES[-1]
